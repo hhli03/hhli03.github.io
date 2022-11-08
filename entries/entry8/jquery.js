5679086397c046a743fd2ad50a3ac7608d576361
hhli03 <lik954@newschool.edu>
2022-11-07 23:49:13 -0500

    entry 8

---
+++ entries/entry8/jquery.js
@@ -0,0 +1,14 @@
+// try playing with this ALERT - 
+// $(document).ready(function() {alert("[Hello! Welcome to SCENT 06!]"); })
+
+// JS files comment with two backslash lines 
+$(document).ready(function() {$("#coffee").draggable(); })
+$(document).ready(function() {$("#boat").draggable(); })
+$(document).ready(function() {$("#sunsetnote").draggable(); })
+$(document).ready(function() {$("#sunset").draggable(); })
+$(document).ready(function() {$("#tube").draggable(); })
+$(document).ready(function() {$("#boatnote").draggable(); })
+$(document).ready(function() {$("#spring").draggable(); })
+$(document).ready(function() {$("#tubenote").draggable(); })
+$(document).ready(function() {$("#cafenote").draggable(); })
+$(document).ready(function() {$("#springnote").draggable(); })
